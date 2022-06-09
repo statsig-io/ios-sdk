89e93c908ac2be301ebefb7fcc20b016f68c7c79
Jiakan Wang <77478330+jkw-statsig@users.noreply.github.com>
2022-06-09 11:47:30 -0700

    support tvos (#121)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Statsig",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v10), .tvOS(.v10)],
     products: [
         .library(
             name: "Statsig",
