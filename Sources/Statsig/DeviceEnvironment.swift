ed5f8d549157941a933ffe340a91a87a76ac85d9
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2023-09-11 15:41:46 -0700

    Bot: Updated File/s [Sources/Statsig/DeviceEnvironment.swift, Statsig.podspec] (#198)

--- Sources/Statsig/DeviceEnvironment.swift
+++ Sources/Statsig/DeviceEnvironment.swift
@@ -6,7 +6,7 @@ struct DeviceEnvironment {
 
     var deviceOS: String = "iOS"
     var sdkType: String = "ios-client"
-    var sdkVersion: String = "1.27.0"
+    var sdkVersion: String = "1.28.0"
     var sessionID: String? { UUID().uuidString }
     var systemVersion: String { PlatformCompatibility.deviceInfo.systemVersion }
     var systemName: String { PlatformCompatibility.deviceInfo.systemName }
