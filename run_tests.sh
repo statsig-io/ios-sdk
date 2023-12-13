d0d29d631cd196067a88e2419d8271685049c581
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2023-12-12 18:46:12 -0800

    Move to self hosted (#227)
    * Move to self hosted
    
    * Remove bad caching
    
    * Update test.yaml
    
    * Use xcodebuild retries

--- run_tests.sh
+++ run_tests.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-MAX_RETRIES=5
-retry=0
-
 cd ".swiftpm/xcode"
 
-while [ $retry -lt $MAX_RETRIES ]; do
-    xcodebuild test \
-        -destination "platform=iOS Simulator,name=iPhone SE (2nd generation)" \
-        -scheme Statsig \
-        -workspace package.xcworkspace \
-        | xcbeautify
-    exit_code=${PIPESTATUS[0]}
-    
-    if [ $exit_code -eq 0 ]; then
-        break
-    else
-        echo "Tests failed with exit code $exit_code. Retrying..."
-        retry=$((retry+1))
-    fi
-done
-
-if [ $retry -eq $MAX_RETRIES ]; then
-    echo "Maximum number of retries reached. Exiting..."
-    exit 1
-else
-    exit 0
-fi
-
+xcodebuild test \
+    -destination "platform=iOS Simulator,name=iPhone SE (3rd generation)" \
+    -scheme Statsig \
+    -workspace package.xcworkspace \
+    -test-iterations 3 \
+    -retry-tests-on-failure \
+    | xcbeautify
