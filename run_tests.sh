3c594b4d6986ee31a5d3ec12f40f26cef4cda2df
Andre Terron <andre@statsig.com>
2025-03-20 13:10:43 -0700

    Only retry tests in GitHub actions (#367)
    After this PR, running `./run_tests.sh` locally will no longer retry
    failed tests. This should make local iteration faster while maintaining
    our CI behavior

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,5 @@ xcodebuild test \
     -destination "platform=iOS Simulator,name=iPhone SE (3rd generation)" \
     -scheme Statsig \
     -workspace package.xcworkspace \
-    -test-iterations 3 \
-    -retry-tests-on-failure \
     "$@" \
     | xcbeautify && exit ${PIPESTATUS[0]}
